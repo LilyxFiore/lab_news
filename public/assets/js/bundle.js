08d4bc48a730c38bb69dce1d712d5f13d0c6c0e8
LilyxFiore <liliana.fiorela@gmail.com>
2017-07-07 15:52:16 -0500

    news secundary js y css

--- public/assets/js/bundle.js
+++ public/assets/js/bundle.js
@@ -50,7 +50,7 @@ function addNew(data) {
   const section = $('<section></section>');
   const row = $('<div class="row"></div>');
   const col = $('<div class="col m12 no-padding pos-relative"></div>');
-  const img = $(' <img src="assets/img/news/' + data[0].img + '" alt="" class="responsive-img">');
+  const img = $(' <img src="assets/img/news/' + data[0].img + '" alt="" class="img-full-w">');
   const newContent = $('<div class="new-main">' +
       '<p class="new-main__title">' + data[0].title + '</p>' +
       '<p class="new-main__text">' + data[0].brief + '</p></div>');
@@ -61,7 +61,28 @@ function addNew(data) {
   col.append(newContent);
 
   $('#root').append(section);
+
+  addSecundary(data);
 }
 
+function addSecundary(data) {
+  console.log(data[0].img);
+  const section = $('<section></section>');
+  const row = $('<div class="row"></div>');
+  const col1 = $('<div class="col m6"><img src="assets/img/news/'+ data[1].img +'" alt="" class="img-full-w">' +
+      '<p class="new-main--dark">'+ data[1].title +'</p>' +
+      '<p class="new-main__hoy">HOY</p>');
+  const col2 =$('<div class="col m3"><img src="assets/img/news/'+ data[2].img +'" alt="" class="img-full-w">' +
+      '<p class="new-main--dark">'+ data[2].title +'</p></div>');
+  const col3 =$('<div class="col m3 pad-left"><img src="assets/img/news/'+ data[3].img +'" alt="" class="img-full-w">' +
+      '<p class="new-main--dark">'+ data[3].title +'</p></div>');
 
+  section.append(row);
+  row.append(col1);
+  row.append(col2);
+  row.append(col3);
+
+  $('#root').append(section);
+
+}
 
