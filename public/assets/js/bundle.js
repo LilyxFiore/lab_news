24509b26bc75a8435f2b7d2a5e45e44ea07938bd
LilyxFiore <liliana.fiorela@gmail.com>
2017-07-07 16:21:46 -0500

    news secundary js y css

--- public/assets/js/bundle.js
+++ public/assets/js/bundle.js
@@ -12,17 +12,19 @@ $('#root').append(newMain());
 /** component header **/
 function header() {
 
-  const header = $('<header></header>');
-  const navTop = $('<nav class="nav-top"><ul class="nav-top_list"><li><img src="assets/img/menu.png" alt="">SECTIONS</li><li><img src="assets/img/search.png" alt="">SEARCH</li> ' +
+  const header = $('<header class="container"></header>');
+  const row = $('<div class="row"></div>');
+  const navTop = $('<nav class="nav-top col s12"><ul class="nav-top_list"><li><img src="assets/img/menu.png" alt="">SECTIONS</li><li><img src="assets/img/search.png" alt="">SEARCH</li> ' +
       '<div class="right"><li><img src="assets/img/fb.png" alt=""></li><li><img src="assets/img/tw.png" alt=""></li><li>' +
       '<img src="assets/img/in.png" alt=""></li></div></ul></nav>');
-  const headerCenter = $('<div class="header-center center-align"><img src="assets/img/logoicon.png" alt=""><p>' + getDate() + ' | <img src="assets/img/cloud.png" alt=""> 23°</p></div>');
-  const navBottom = $('<nav class="nav-bottom"><ul class="nav-bottom__list"><li>Lo último</li><li>Opinión</li><li>Cultura</li><li>Perú</li>' +
+  const headerCenter = $('<div class="header-center center-align col s12"><img src="assets/img/logoicon.png" alt=""><p>' + getDate() + ' | <img src="assets/img/cloud.png" alt=""> 23°</p></div>');
+  const navBottom = $('<nav class="nav-bottom col s12"><ul class="nav-bottom__list"><li>Lo último</li><li>Opinión</li><li>Cultura</li><li>Perú</li>' +
       '<li>Tecnología</li><li>Mundo</li><li>Economía</li><li>Lifestyle</li><li>Deporte</li></ul></nav>');
 
-  header.append(navTop);
-  header.append(headerCenter);
-  header.append(navBottom);
+  header.append(row);
+  row.append(navTop);
+  row.append(headerCenter);
+  row.append(navBottom);
 
   return header;
 }
@@ -47,13 +49,13 @@ function newMain(){
 
 function addNew(data) {
   console.log(data[0].img);
-  const section = $('<section></section>');
+  const section = $('<section class="container"></section>');
   const row = $('<div class="row"></div>');
-  const col = $('<div class="col m12 no-padding pos-relative"></div>');
-  const img = $(' <img src="assets/img/news/' + data[0].img + '" alt="" class="img-full-w">');
+  const col = $('<div class="col s12 m12 pos-relative m-top"></div>');
+  const img = $(' <img src="assets/img/news/' + data[0].img + '" alt="" class="responsive-img">');
   const newContent = $('<div class="new-main">' +
       '<p class="new-main__title">' + data[0].title + '</p>' +
-      '<p class="new-main__text">' + data[0].brief + '</p></div>');
+      '<p class="new-main__text hide-on-small-only">' + data[0].brief + '</p></div>');
 
   section.append(row);
   row.append(col);
@@ -67,14 +69,14 @@ function addNew(data) {
 
 function addSecundary(data) {
   console.log(data[0].img);
-  const section = $('<section></section>');
+  const section = $('<section class="container"></section>');
   const row = $('<div class="row"></div>');
-  const col1 = $('<div class="col m6"><img src="assets/img/news/'+ data[1].img +'" alt="" class="img-full-w">' +
+  const col1 = $('<div class="col s12 m6"><img src="assets/img/news/'+ data[1].img +'" alt="" class="img-full-w">' +
       '<p class="new-main--dark">'+ data[1].title +'</p>' +
       '<p class="new-main__hoy">HOY</p>');
-  const col2 =$('<div class="col m3"><img src="assets/img/news/'+ data[2].img +'" alt="" class="img-full-w">' +
+  const col2 =$('<div class="col s12 m3"><img src="assets/img/news/'+ data[2].img +'" alt="" class="img-full-w">' +
       '<p class="new-main--dark">'+ data[2].title +'</p></div>');
-  const col3 =$('<div class="col m3 pad-left"><img src="assets/img/news/'+ data[3].img +'" alt="" class="img-full-w">' +
+  const col3 =$('<div class="col s12 m3"><img src="assets/img/news/'+ data[3].img +'" alt="" class="img-full-w">' +
       '<p class="new-main--dark">'+ data[3].title +'</p></div>');
 
   section.append(row);
@@ -85,4 +87,3 @@ function addSecundary(data) {
   $('#root').append(section);
 
 }
-
--- src/assets/js/components/news-main.js
+++ src/assets/js/components/news-main.js
@@ -10,13 +10,13 @@ function newMain(){
 
 function addNew(data) {
   console.log(data[0].img);
-  const section = $('<section></section>');
+  const section = $('<section class="container"></section>');
   const row = $('<div class="row"></div>');
-  const col = $('<div class="col m12 no-padding pos-relative"></div>');
-  const img = $(' <img src="assets/img/news/' + data[0].img + '" alt="" class="img-full-w">');
+  const col = $('<div class="col s12 m12 pos-relative m-top"></div>');
+  const img = $(' <img src="assets/img/news/' + data[0].img + '" alt="" class="responsive-img">');
   const newContent = $('<div class="new-main">' +
       '<p class="new-main__title">' + data[0].title + '</p>' +
-      '<p class="new-main__text">' + data[0].brief + '</p></div>');
+      '<p class="new-main__text hide-on-small-only">' + data[0].brief + '</p></div>');
 
   section.append(row);
   row.append(col);
@@ -30,14 +30,14 @@ function addNew(data) {
 
 function addSecundary(data) {
   console.log(data[0].img);
-  const section = $('<section></section>');
+  const section = $('<section class="container"></section>');
   const row = $('<div class="row"></div>');
-  const col1 = $('<div class="col m6"><img src="assets/img/news/'+ data[1].img +'" alt="" class="img-full-w">' +
+  const col1 = $('<div class="col s12 m6"><img src="assets/img/news/'+ data[1].img +'" alt="" class="img-full-w">' +
       '<p class="new-main--dark">'+ data[1].title +'</p>' +
       '<p class="new-main__hoy">HOY</p>');
-  const col2 =$('<div class="col m3"><img src="assets/img/news/'+ data[2].img +'" alt="" class="img-full-w">' +
+  const col2 =$('<div class="col s12 m3"><img src="assets/img/news/'+ data[2].img +'" alt="" class="img-full-w">' +
       '<p class="new-main--dark">'+ data[2].title +'</p></div>');
-  const col3 =$('<div class="col m3 pad-left"><img src="assets/img/news/'+ data[3].img +'" alt="" class="img-full-w">' +
+  const col3 =$('<div class="col s12 m3"><img src="assets/img/news/'+ data[3].img +'" alt="" class="img-full-w">' +
       '<p class="new-main--dark">'+ data[3].title +'</p></div>');
 
   section.append(row);
